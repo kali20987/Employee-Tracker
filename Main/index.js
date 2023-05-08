d542b428b5219158a9149993a0f6c500ce177ff1
kali19 <khadijah.syed.ali@gmail.com>
2023-05-08 15:36:51 -0400

    added mysql package

--- Main/index.js
+++ Main/index.js
@@ -1,10 +1,22 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: 'vad_db'
+    },
+    console.log(`Connected to the vad_db database.`)
+  );
+
 inquirer
     .prompt([
         {
             type: 'list',
             message: 'What would you like to do?',
-            name: 'user',
+            name: 'firstUserPrompt',
             choices: ['View all Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
         },
     ])
